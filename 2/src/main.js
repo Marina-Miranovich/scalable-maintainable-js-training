c530aaca1a9231b4525c80ea5d578ce278cc9b66
Marina <freemyska666@gmail.com>
2014-01-20 14:16:07 +0300

    + bower

--- 2/src/main.js
+++ 2/src/main.js
@@ -1,11 +1,19 @@
 define(['event-bus', 'quiz-module', 'jquery', 'require', 'yepnope', 'modernizr'], function(EventBus, quiz, $, require, yepnope, Modernizr) {
 
+	//Bower
+	//Backbone
+	//Collection for the questions
+	//Model for the answers
+	//View: rendering, binding
+	//Mixin backbone's events
+
+
 	return {
 		init: function () {
 			yepnope({
 				'test': Modernizr.json,
-				'yep': '',
-				'nope': 'json2'
+				'yep': '2/src/json2.js',
+				'nope': ''
 			});
 			$('.quizContainer').createQuiz();
 			EventBus.bind('quizFinished', this.loadAnswers.bind(this));
